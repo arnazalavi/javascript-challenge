a4679d41179589af2b37c33ac0075bc81d6d5654
arnazalavi <arnazalavi@gmail.com>
2021-10-03 22:54:26 -0500

    Latest file on 10/3

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+var botton = d3.select("#filter-btn");
+
+
 // YOUR CODE HERE!
+//console.log(tableData);
+
+//Create event handlers
+botton.on("click", runEnter);
+
+//d3.event.preventDefault();
+
+//Step 5
+// Complete the event handler function for the form
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select('#datetime')
+   var inputValue = inputElement.property("value");
+
+   var cityElement = d3.select('#city')
+   var inputCity = cityElement.property("value");
+ 
+   console.log(inputValue);
+   var filteredData = tableData.filter(data => data.datetime == inputValue || data.city == inputCity) ;  
+
+   loadTable(filteredData);
+}
+
+function loadTable(table){
+    tbody.html("")
+    // datetime
+    //var filteredData = tableData.filter(dateEnter => dateEnter.datetime == inputValue);
+     //var filteredData = tableData.filter(dateEnter => dateEnter.datetime == inputValue);
+   
+   table.forEach(function(UFO) {
+
+        console.log(UFO);
+        var row = tbody.append("tr");
+       
+       Object.entries(UFO).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+        cell.text(value);
+          })
+        });
+    
+}
+
+
+ loadTable(tableData)
